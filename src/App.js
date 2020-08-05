f353d9ecccf89546b4c580bf876fc7be3364a964
Alex Ford <gordion42@gmail.com>
2020-08-05 16:14:03 -0600

    Initial react.js template

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <h1>Under Construction</h1>
+        <h3>This site is currently under construction, please excuse our dust</h3>
+    </div>
+  );
+}
+
+export default App;

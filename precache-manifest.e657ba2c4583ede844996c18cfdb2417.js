84239c8f1d069a4bd3a7a837ed8881a1077683cf
thePalindrome <thePalindrome@users.noreply.github.com>
2020-08-05 22:41:49 +0000

    deploy: 8a073cd848fc473d665cf7125cfaab987fe8296f

---
+++ precache-manifest.e657ba2c4583ede844996c18cfdb2417.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc9e26ad8ecaed9a14fbd6735b8eb18f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7e0bb3111ce80b24e5fc",
+    "url": "/static/css/main.4bc599af.chunk.css"
+  },
+  {
+    "revision": "925df787eebd3fd5e9f6",
+    "url": "/static/js/2.74b8be5c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.74b8be5c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e0bb3111ce80b24e5fc",
+    "url": "/static/js/main.277dcfe9.chunk.js"
+  },
+  {
+    "revision": "ffc6a7cc91d7b7abc8cb",
+    "url": "/static/js/runtime-main.1d063100.js"
+  }
+]);
